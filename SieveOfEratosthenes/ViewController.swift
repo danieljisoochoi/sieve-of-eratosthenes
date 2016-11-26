e34d0204a731c71d16d0651c72822ea075c418a7
Daniel J Choi <git@bitbucket.org:jisoochoi/block2.0.git>
2016-11-25 22:27:14 -0800

    Initial Commit

---
+++ SieveOfEratosthenes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SieveOfEratosthenes
+//
+//  Created by Daniel Jisoo Choi on 11/25/16.
+//  Copyright © 2016 Daniel Jisoo Choi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
